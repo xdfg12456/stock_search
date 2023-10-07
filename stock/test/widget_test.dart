0de9b869e815e1349dd11ee74d306c987f636bda
xdfg12456 <xdfg12456@gmail.com>
2023-10-07 19:42:21 +0800

    fix ci

--- stock/test/widget_test.dart
+++ stock/test/widget_test.dart
@@ -11,5 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:stock/main.dart';
 
 void main() {
-  
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+ 
+  });
 }
